711a4c65ed19041ac3e8fda0bd7b9fe951181ffd
Florine Lefebvre <florine.lefebvre.etu@univ-lille.fr>
2024-09-03 10:28:45 +0200

    initialisation plateaux et bateaux main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,31 @@ package main;
  * Main
  */
 public class Main {
+
+    final static String CLEAR = String.format("\033[2J");
+
     public static void main(String[] args) {
-        System.out.print(String.format("\033[2J"));
-        Plateau plt = new Plateau(true);
-        System.out.println(plt.getStringPlateau());
+        System.out.print(CLEAR);
+
+        //création des plateaux
+        Plateau playerPlateau = new Plateau(true);
+        System.out.println(playerPlateau.getStringPlateau());
+
+        Plateau botPlateau = new Plateau(false);
+        System.out.println(botPlateau.getStringPlateau());
+
+        //création des bateaux du joueur
+        Croiseur playerCroiseur = new Croiseur();
+        Croiseur playerCroiseur2 = new Croiseur();
+        Cuirasser playerCuirasser = new Cuirasser();
+        Destroyer playerDestroyer = new Destroyer();
+        PorteAvion playerPorteAvion = new PorteAvion();
+
+        //création des bateaux du bot
+        Croiseur botCroiseur = new Croiseur();
+        Croiseur botCroiseur2 = new Croiseur();
+        Cuirasser botCuirasser = new Cuirasser();
+        Destroyer botDestroyer = new Destroyer();
+        PorteAvion botPorteAvion = new PorteAvion();
     }
 }
