f828d7c4997d16622c2b94bf77e6f39143c004aa
Louis BEDU <louis.bedu.etu@univ-lille.fr>
2024-09-03 10:52:09 +0200

    Add setBateau

--- src/main/Plateau.java
+++ src/main/Plateau.java
@@ -19,23 +19,43 @@ public class Plateau {
     public String getStringPlateau() {
         String res = new String();
         char lettre = 'A';
-        for (int i = 0; i < this.plt.length; i++) {
-            res += lettre;
-            for (int j = 0; j < this.plt[i].length; j++) {
-                if (this.plt[i][j] == null) {
-                    res += "|  ";
-                } else {
-                    res += "|" + this.plt[i][j];
+        if (this.our) {
+            for (int i = 0; i < this.plt.length; i++) {
+                res += lettre;
+                for (int j = 0; j < this.plt[i].length; j++) {
+                    if (this.plt[i][j] == null) {
+                        res += "|  ";
+                    } else {
+                        res += "|  ";
+                    }
                 }
+                lettre++;
+                res += "|\n";
             }
-            lettre++;
-            res += "|\n";
+        } else {
+            for (int i = 0; i < this.tire.length; i++) {
+                res += lettre;
+                for (int j = 0; j < this.tire[i].length; j++) {
+                    if (this.plt[i][j] == null) {
+                        res += "|  ";
+                    } else {
+                        res += "| x";
+                    }
+                }
+                lettre++;
+                res += "|\n";
+            }
+
         }
         return res + " | 1| 2| 3| 4| 5| 6| 7| 8| 9|10|";
     }
 
     public Bateau[][] getPlateau() {
         return this.plt;
+    }
+
+    public void setBateau(int x, int y, Bateau bt) {
+        this.plt[x][y] = bt;
     }
 
     public String getTour() {
