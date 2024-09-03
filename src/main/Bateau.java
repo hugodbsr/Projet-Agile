fa5e7afe388c7783ec2d112e0ac3f32e166e5b48
Louis BEDU <louis.bedu.etu@univ-lille.fr>
2024-09-03 09:03:23 +0200

    Update Plateau

---
+++ src/main/Bateau.java
@@ -0,0 +1,8 @@
+package main;
+
+/**
+ * Bateau
+ */
+public interface Bateau {
+
+}
--- src/main/Plateau.java
+++ src/main/Plateau.java
@@ -4,7 +4,29 @@ package main;
  * Plateau
  */
 public class Plateau {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private Bateau plt[][];
+    private int tour;
+
+    public Plateau() {
+        this.plt = new Bateau[10][10];
+        this.tour = 0;
+    }
+
+    public String getPlateau() {
+        String res = new String();
+        for (int i = 0; i < this.plt.length; i++) {
+            for (int j = 0; j < this.plt[i].length; j++) {
+                res += this.plt[i][j];
+            }
+        }
+        return res;
+    }
+
+    public String getTour() {
+        return "Tour n°" + this.tour;
+    }
+
+    public void reset() {
+        this.plt = new Bateau[10][10];
     }
 }
