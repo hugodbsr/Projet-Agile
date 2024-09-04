d4a8883772f568ba6f94ecb964dce071b29a26e9
hugo <hugo.debuyser.etu@univ-lille.fr>
2024-09-04 11:21:14 +0200

    Correction bug et affichage saisie

--- src/main/Saisie.java
+++ src/main/Saisie.java
@@ -25,7 +25,7 @@ public class Saisie {
         String texte = "";
         while(!correct){
             texte = getSaisie();
-            if(texte.length()>1 || texte.length()<4){
+            if(texte.length()>1 && texte.length()<4){
                 if((texte.charAt(0)>='A' && texte.charAt(0)<='Z') || (texte.charAt(0)>='a' && texte.charAt(0)<='z')){
                     texte = Character.toUpperCase(texte.charAt(0)) + texte.substring(1);
                     if((texte.charAt(1)>='1' && texte.charAt(1)<='9')){
@@ -39,9 +39,10 @@ public class Saisie {
                             return texte;
                         }
                     }
-                }
-                System.err.println("Veillez entrer des coordonnées de tir dans le bon format (ex : A5)");       
+                }  
             }
+            System.err.println("Veillez entrer des coordonnées de tir dans le bon format (ex : A5)");
+            System.out.print("Entrez les coordonnées de tir (ex : A5) : "); 
         }
         return texte;
     }
