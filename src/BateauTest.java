baeb28b8a294aa305f80f207fbbf00c3a8d9cf90
Florine Lefebvre <florine.lefebvre.etu@univ-lille.fr>
2024-09-04 08:55:36 +0200

    modif bateau + bateautest

--- src/BateauTest.java
+++ src/BateauTest.java
@@ -1,10 +1,11 @@
-package main;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import main.Bateau;
+import main.Cuirasser;
 
 public class BateauTest {
     @Test
