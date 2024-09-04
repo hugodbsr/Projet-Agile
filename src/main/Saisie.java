e765b0819e3e4cc19264091585627675cb2f6f77
hugo <hugo.debuyser.etu@univ-lille.fr>
2024-09-04 08:58:45 +0200

    Ajout d'une méthode de vérification de la saisie des coordonnées de tir

--- src/main/Saisie.java
+++ src/main/Saisie.java
@@ -18,4 +18,26 @@ public class Saisie {
         }
         return getSaisie();
     }
+
+    public static String getPositionTir(){
+        boolean correct = false;
+        String texte = "";
+        while(!correct){
+            texte = getSaisie();
+            if(texte.length()>1 || texte.length()<4){
+                if((texte.charAt(0)>='A' && texte.charAt(0)>='Z') || (texte.charAt(0)>='a' && texte.charAt(0)>='z')){
+                    texte = Character.toUpperCase(texte.charAt(0)) + texte.substring(1);
+                    if((texte.charAt(1)>='1' && texte.charAt(1)>='9')){
+                        if((texte.length()==3) && texte.charAt(1)=='1'){
+                            if((texte.charAt(2)=='0')){
+                                correct = true;
+                            }
+                        }
+                    }
+                }
+                System.err.println("Veillez entrer des coordonnées de tir dans le bon format (ex : A5)");             
+            }
+        }
+        return texte;
+    }
 }
