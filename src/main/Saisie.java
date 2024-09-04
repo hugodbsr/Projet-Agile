73738bc4dffba9e1db2fd1e4b79c7ff47853c07d
Thomas Smeeckaert <thomas.smeeckaert.etu@univ-lille.fr>
2024-09-04 08:22:00 +0200

    catch exception saisie

--- src/main/Saisie.java
+++ src/main/Saisie.java
@@ -1,12 +1,21 @@
 package main;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Saisie {
 
     public static String getSaisie(){
-        Scanner sc = new Scanner(System.in);
-        String texte = sc.next();
-        return texte;
+        try (Scanner sc = new Scanner(System.in)){
+            String texte = sc.next();
+            return texte;
+        } catch (IllegalStateException e) {
+            System.out.println(e.getStackTrace());
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getStackTrace());
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return getSaisie();
     }
 }
