cfb32bfb1d1d3411e5f82f8b4e7019652986fe2c
Thomas Smeeckaert <thomas.smeeckaert.etu@univ-lille.fr>
2024-09-04 09:37:03 +0200

    Position Exception

---
+++ src/main/WrongPositionException.java
@@ -0,0 +1,16 @@
+package main;
+
+public class WrongPositionException extends Exception {
+    public WrongPositionException(){
+        System.out.println("Mauvais placement d'un bateau !");
+    }
+
+    public WrongPositionException(String s){
+        System.out.println(s);
+    }
+
+    public WrongPositionException(String s, Throwable t){
+        System.out.println(s);
+        System.out.println(t);
+    }
+}
