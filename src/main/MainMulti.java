2e97fe5022712652d40e3b4fb1d77392182c80dd
Louis BEDU <louis.bedu.etu@univ-lille.fr>
2024-09-05 10:10:08 +0200

    Update gitignore + Multi

--- src/main/MainMulti.java
+++ src/main/MainMulti.java
@@ -12,12 +12,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class MainMulti {
     private final static String PATH = "res/main/";
-    private static String room, player1, player2, currentPlayer;
-    final static String CLEAR = String.format("\033[2J");
+    private static String room, player1, player2, currentPlayer, roomFile;
+    private final static String CLEAR = String.format("\033[2J");
+    private static Plateau ourPlt1, ourPlt2, plt1, plt2;
 
-    public static String loadFile() {
+    public static String loadFile(String file) {
         String res = new String();
-        try (BufferedReader br = new BufferedReader(new FileReader(new File(PATH + "Multi.csv")))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(PATH + file)))) {
             while (br.ready()) {
                 res += br.readLine() + '\n';
             }
@@ -29,7 +30,7 @@ public class MainMulti {
     }
 
     public static int checkRoom(String name) {
-        String rooms = loadFile();
+        String rooms = loadFile("Multi.csv");
         String[] tab = rooms.split("\n");
         for (int i = 0; i < tab.length; i++) {
             if (tab[i].split(";")[0].equals(name)) return i;
@@ -38,7 +39,7 @@ public class MainMulti {
     }
 
     public static boolean createGame(String room, String player1) {
-        String multi = loadFile();
+        String multi = loadFile("Multi.csv");
         try {
             FileWriter writer = new FileWriter(PATH + "Multi.csv");
             writer.write(multi + room + ";" + player1 + ";");
@@ -52,7 +53,7 @@ public class MainMulti {
 
     public static boolean addPlayer(int idx, String player2) {
         try {
-            String multi = loadFile();
+            String multi = loadFile("Multi.csv");
             String[] tab = multi.split("\n");
             if (tab[idx].split(";").length == 3) throw new RoomFullException("\u001B[31mLa salle est remplie !\u001B[00m");
             tab[idx] = tab[idx].split(";")[0] + ";" + tab[idx].split(";")[1] + ";" + player2;
@@ -67,7 +68,6 @@ public class MainMulti {
             System.err.println(e.getMessage());
             System.exit(1);
         } catch (Exception e) {
-            System.out.println(e);
             System.err.println("\u001B[31mUne erreur est survenue veuillez contacter le support !\u001B[00m");
             System.exit(1);
         }
@@ -75,7 +75,7 @@ public class MainMulti {
     }
 
     public static boolean checkPLayer2() {
-        String[] rooms = loadFile().split("\n");
+        String[] rooms = loadFile("Multi.csv").split("\n");
         int idx = checkRoom(room);
         if (rooms[idx].split(";").length == 3) return true;
         return false;
@@ -100,10 +100,85 @@ public class MainMulti {
             System.err.println("\u001B[31mUne erreur est survenue veuillez contacter le support !\u001B[00m");
             System.exit(1);
         }
+        createRoom();
+        System.out.println(CLEAR);
+        System.out.println("\u001B[31m" + loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[1] + "\u001B[00m vs \u001B[34m" + loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[2] + "\u001B[00m");
+        ourPlt1 = new Plateau(true);
+        plt2 = new Plateau(false);
+        String posBat = Saisie.getPositionBateau();
+        if (posBat.equals("R")) {
+            DeroulementDuJeu.placeShipsRandomly(ourPlt1);
+        } else {
+            DeroulementDuJeu.placeShipsManually(ourPlt1, plt2);
+        }
+
+        try {  
+            String roomCsv = loadFile(roomFile + ".csv");
+            String tab = new String();
+            for (Bateau[] iterable_element : ourPlt1.getPlateau()) {
+                for (Bateau bateau : iterable_element) {
+                    if (bateau == null) {
+                        tab += bateau + "/";
+                    } else {
+                        tab += bateau.getClass().getSimpleName() + "/";
+                    }
+                }
+            }
+            tab = tab.substring(0, tab.length() - 1);
+            String res = roomCsv.split("\n")[0].split(";")[0] + ";" + tab + ";;\n" + roomCsv.split("\n")[1];
+            FileWriter writer = new FileWriter(PATH + roomFile + ".csv");
+            writer.write(res);
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("\u001B[31mUne erreur est survenue veuillez contacter le support !\u001B[00m");
+            System.exit(1);
+        }
     }
     
     public static void gamePlayer2() {
         System.out.println(CLEAR);
+        System.out.println("\u001B[31m" + loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[1] + "\u001B[00m vs \u001B[34m" + loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[2] + "\u001B[00m");
+        ourPlt2 = new Plateau(true);
+        plt1 = new Plateau(false);
+        String posBat = Saisie.getPositionBateau();
+        if (posBat.equals("R")) {
+            DeroulementDuJeu.placeShipsRandomly(ourPlt2);
+        } else {
+            DeroulementDuJeu.placeShipsManually(ourPlt2, plt1);
+        }
+
+        try {  
+            String roomCsv = loadFile(roomFile + ".csv");
+            String tab = new String();
+            for (Bateau[] iterable_element : ourPlt2.getPlateau()) {
+                for (Bateau bateau : iterable_element) {
+                    if (bateau == null) {
+                        tab += bateau + "/";
+                    } else {
+                        tab += bateau.getClass().getSimpleName() + "/";
+                    }
+                }
+            }
+            tab = tab.substring(0, tab.length() - 1);
+            String res = roomCsv.split("\n")[0] + "\n" + roomCsv.split("\n")[1].split(";")[0] + ";" + tab + ";;";
+            FileWriter writer = new FileWriter(PATH + roomFile + ".csv");
+            writer.write(res);
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("\u001B[31mUne erreur est survenue veuillez contacter le support !\u001B[00m");
+            System.exit(1);
+        }
+    }
+
+    public static void createRoom() {
+        try {  
+            FileWriter writer = new FileWriter(PATH + "R" + room + ".csv");
+            writer.write(loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[1] + ";;;\n" + loadFile("Multi.csv").split("\n")[checkRoom(room)].split(";")[2] + ";;;");
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("\u001B[31mUne erreur est survenue veuillez contacter le support !\u001B[00m");
+            System.exit(1);
+        }
     }
 
     public static void main(String[] args) {
@@ -111,6 +186,7 @@ public class MainMulti {
         room = Saisie.getSaisie().toLowerCase();
         System.out.print("Quelle est votre pseudo ? : ");
         int roomIdx = checkRoom(room);
+        roomFile = "Rooms/R" + room;
         if (roomIdx == -1) {
             player1 = Saisie.getSaisie().toLowerCase();
             createGame(room, player1);
